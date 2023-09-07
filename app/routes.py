33013895a8453140740035b6c1a2cbc04b65e110
Dvwhite81 <Dvwhite1981@gmail.com>
2023-09-07 16:32:48 -0400

    Main route working with psycopg2

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,26 @@
 from flask import Blueprint, render_template
+import os
+import psycopg2, psycopg2.extras
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
-@bp.route('/')
+CONNECTION_PARAMETERS = {
+    "user": os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASS"),
+    "dbname": os.environ.get("DB_NAME"),
+    "host": os.environ.get("DB_HOST"),
+}
+
+@bp.route("/")
 def main():
-    return render_template('main.html')
+    with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as curs:
+            curs.execute('''
+                         SELECT id, name, start_datetime, end_datetime
+                         FROM appointments
+                         ORDER BY start_datetime;
+                         ''')
+            rows = curs.fetchall()
+            for row in rows:
+                print(row)
+            return render_template("main.html", rows=rows)
